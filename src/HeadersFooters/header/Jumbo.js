a1158513bbfa11541b99acafa0b14ae9ad9b452b
kjay <kcdhopkins@gmail.com>
2020-02-05 06:37:13 -0600

    Upadtes added to jumbotron, SPA

--- src/HeadersFooters/header/Jumbo.js
+++ src/HeadersFooters/header/Jumbo.js
@@ -1,7 +1,5 @@
 import React from "react";
 import backgroundImage from '../../assets/img/jumbotron.jpg';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 const Jumbo = () => {
   const jumboStyle = {
@@ -19,8 +17,8 @@ const Jumbo = () => {
         Web developer with front and backend experience using html, css, vanilla/modern/bootstrap javascript on the frontend, php and mysql on the backend to provide the optimal user experience.
       </p>
       <div>
-        <a style ={buttonStyle} className='btn btn-outline-light' href="https://github.com/kcdhopkins" role='button'><FontAwesomeIcon icon={faBars}/> Github</a>
-        <a style ={buttonStyle} className='btn btn-outline-light' href="https://www.linkedin.com/in/keyairius-hopkins-4b7b984a" role='button'><FontAwesomeIcon icon={faBars}/> LinkedIn</a>
+        <a style ={buttonStyle} className='btn btn-outline-light' href="https://github.com/kcdhopkins" role='button'> Github</a>
+        <a style ={buttonStyle} className='btn btn-outline-light' href="https://www.linkedin.com/in/keyairius-hopkins-4b7b984a" role='button'>LinkedIn</a>
       </div>
     </div>
   );
